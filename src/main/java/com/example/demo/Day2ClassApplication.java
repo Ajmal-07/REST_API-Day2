7753143fa72bfbc7388a8e1014c22da144ac56f6
Ajmal-07 <727721euec090@skcet.ac.in>
2023-08-18 15:04:41 +0530

    first commit

---
+++ src/main/java/com/example/demo/Day2ClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2ClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2ClassApplication.class, args);
+	}
+
+}
